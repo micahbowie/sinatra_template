efd6a94c0848cb2453bdad4716423e53baeafaef
Micah Bowie <micahbowie20@gmail.com>
2020-09-27 20:15:39 -0400

    creating relationships and tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200924011620) do
+ActiveRecord::Schema.define(version: 20200927235025) do
+
+  create_table "notes", force: :cascade do |t|
+    t.string  "title"
+    t.string  "content"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
